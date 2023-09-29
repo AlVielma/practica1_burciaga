a82dce7e72e0ccfacf215b89e903582c7a3606c3
AlVielma <vielma7220@gmail.com>
2023-09-28 19:31:40 -0600

    Que sea lo que dios quiera

---
+++ ExpresionRegular.java
@@ -0,0 +1,118 @@
+public class ExpresionRegular {
+    private String cadena;
+
+    public ExpresionRegular(String cadena) {
+        this.cadena = cadena;
+    }
+
+    // metodo principal que verifica si la cadena cumple alguna de las condiciones.
+    public boolean cumpleCondiciones() {
+        return cumpleCondiciones1() || cumpleCondiciones2() || contieneSecuenciaDigitos1() || contieneSecuenciaDigitos();
+    }
+
+     // vemos si la cadena tiene @# vocal mayúscula
+    public boolean cumpleCondiciones1() {
+        char[] chars = cadena.toCharArray();
+        int length = cadena.length();
+    
+        if (length != 3) {
+            return false;
+        }
+    
+        for (int i = 0; i < length; i++) {
+            char c = chars[i];
+            if (i == 0 && c != '@') {
+                return false;
+            } else if (i == 1 && c != '#') {
+                return false;
+            } else if (i == 2 && !("AEIOU".contains(String.valueOf(c)))) {
+                return false; 
+            }
+        }
+    
+        return true;
+    }
+
+    // verificamos si tiene $ vocal minúscula
+    public boolean cumpleCondiciones2() {
+        char[] chars = cadena.toCharArray();
+        int length = cadena.length();
+
+        if (length != 2) {
+            return false;
+        }
+
+        for (int i = 0; i < length; i++) {
+            char c = chars[i];
+            if (i == 0 && c != '$') {
+                return false;
+            } else if (i == 1 && !"aeiou".contains(String.valueOf(c))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+     // verificamos si la cadena tiene el formato '$ dígitos impar vocal mayúscula vocal minúscula'.
+    public boolean contieneSecuenciaDigitos() {
+        char[] chars = cadena.toCharArray();
+        int length = cadena.length();
+
+        if (length < 6) {
+            return false;
+        }
+
+        for (int i = 0; i < length; i++) {
+            char c = chars[i];
+
+            if (i == 0 && c != '$') {
+                return false;
+            } else if (i == 1 && !Character.isDigit(c)) {
+                return false;
+            } else if (i == 2 && !Character.isDigit(c)) {
+                return false;
+            } else if (i == 3 && (c != '1' && c != '3' && c != '5' && c != '7' && c != '9')) {
+                return false;
+            } else if (i == 4 && !"AEIOU".contains(String.valueOf(c))) {
+                return false;
+            } else if (i == 5 && !"aeiou".contains(String.valueOf(c))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // verificamos si la cadena tiene el formato @# dígitos impar y las 2 vocales mayusculas
+    public boolean contieneSecuenciaDigitos1() {
+        char[] chars = cadena.toCharArray();
+        int length = cadena.length();
+
+        if (length < 6) {
+            return false;
+        }
+
+        for (int i = 0; i < length; i++) {
+            char c = chars[i];
+
+            if (i == 0 && c != '@') {
+                return false;
+            } else if (i == 1 && c != '#') {
+                return false;
+            } else if (i == 2 && !Character.isDigit(c)) {
+                return false;
+            } else if (i == 3 && !Character.isDigit(c)) {
+                return false;
+            } else if (i == 4 && (c != '1' && c != '3' && c != '5' && c != '7' && c != '9')) {
+                return false;
+            } else if (i == 5 && !"AEIOU".contains(String.valueOf(c))) {
+                return false;
+            } else if (i == length - 1 && !"AEIOU".contains(String.valueOf(c))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ probar.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class probar {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        // Empezamos con un do while para que se haga el sistema de menu en consola
+        do {
+            System.out.println("Bienvenido a nuestra expresión regular:");
+            System.out.println("1. Ingresar la cadena y verificar");
+            System.out.println("2. Ver créditos");
+            System.out.println("3. Salir");
+            System.out.print("Ingresa el número de opción: ");
+            
+            // declaramos nuestro escaner por que si no, no podras escribir
+            opcion = scanner.nextInt();
+
+            scanner.nextLine(); // Limpia el buffer del teclado
+
+            switch (opcion) {
+                //si el usuaqio selecciona 1 hace la expresion regular
+                case 1:
+                    System.out.print("Ingresa una cadena: ");
+                    String cadena = scanner.nextLine();
+                    //mandamos la cadena a nuestra clase
+                    ExpresionRegular reconocedor = new ExpresionRegular(cadena);
+                    //mediante las condiciones que tenemos en las funciones, dira si es verdadera o no
+                    if (reconocedor.cumpleCondiciones()) {
+                        System.out.println("La cadena SI cumple con las condiciones.");
+                    } else {
+                        System.out.println("La cadena NO cumple con las condiciones.");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Créditos: Este programa fue desarrollado por:");
+                    System.out.println("Francisco Javier Vielma Padron 22170029");
+                    System.out.println("Jorge Adrian Lira Lopez (tu matricula)");
+                    break;
+                case 3:
+                    System.out.println("Gracias por comprobrar nuestra expresión regular, cualquier comentario es recibido :)");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, selecciona una opción válida.");
+            }
+        } while (opcion != 3);
+
+        scanner.close();
+    }
+}
